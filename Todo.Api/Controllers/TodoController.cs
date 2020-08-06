0ebd2397644f9d5675345901cc61d2bdc3a2df2e
legen <504883942@qq.com>
2020-08-06 16:40:44 +0800

    修改控制器的默认路由
    为了便于理解，我们将默认路由改成
    api/[controller]/[action]

--- Todo.Api/Controllers/TodoController.cs
+++ Todo.Api/Controllers/TodoController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Todo.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class TodoController : ControllerBase
     {
