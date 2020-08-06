7f34e33d3c112b9a5eba322ac798c027f71d4755
legen <504883942@qq.com>
2020-08-06 17:26:15 +0800

    创建dto
    action接受复杂参数时，往往会用一个对象来接收

---
+++ Todo.Api/Dto/AddItemInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Api.Dto
+{
+    /// <summary>
+    /// 用于添加todo项的输入
+    /// </summary>
+    public class AddItemInput
+    {
+        /// <summary>
+        /// 待添加的项所属的list
+        /// </summary>
+        public Guid Uid { get; set; }
+        /// <summary>
+        /// 代办事项的内容
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
